f14ab8f2c30dded257f71a807d063f89690a297b
seunghyun <inyo0506@gmail.com>
2023-08-25 18:08:51 +0900

    08.25수정

--- frontend/js/game.js
+++ frontend/js/game.js
@@ -4,6 +4,7 @@ const main = document.querySelector('main')
 let charIndex = 0
 let index = 1
 let serverData = []
+let newArr = [] 
 
 // 성경 서버데이터 가져오기
 async function getBibleData(){
@@ -100,7 +101,6 @@ async function getBibleText(){
             bibleText.appendChild(biblePargraph)
        }
        }
-       console.log(bibleText.innerHTML)
 // 시편본문 한글자씩 풀어서 span태그로 감싸주기   
     const bibletextPara = typingContent.querySelectorAll('.bible-Text p') 
 
@@ -109,29 +109,35 @@ async function getBibleText(){
     }
     
 const textSpan = typingContent.querySelectorAll('span')
-
 // 텍스트 입력창 글자입력 오류검증 기능
-textWindow.addEventListener('keyup',e=>{   
+textWindow.addEventListener('keyup',e=>{  
+    console.log(textSpan[0])
     const inputSpanText = e.target.value
     inputDiv.innerText = inputSpanText
     let typedText = inputSpanText.split('')
-    
-    if(textSpan[charIndex].innerText === typedText[charIndex]){ // 글자가 일치할 경우
+    console.log( textSpan[charIndex].innerText, typedText[charIndex])
+    if(typedText.length){ // 글자 타이핑할때 span 숨기기
+        textSpan[charIndex].classList.add('hide')}
+
+    if(textSpan[charIndex]?.innerText === typedText[charIndex]){ // 글자가 일치할 경우
         textSpan[charIndex].innerText = typedText[charIndex]
         textSpan[charIndex].classList.add('correct')
         textSpan[charIndex].classList.remove('incorrect')
         charIndex++ 
     e.preventDefault()
-
     }
     else if(typedText[charIndex] == null){  // 글자를 지울 때
-        textSpan[charIndex].classList.remove('correct')
-        textSpan[charIndex].classList.remove('incorrect')
-        if(charIndex > 0) charIndex-- 
+        textSpan[charIndex].classList.remove('correct','incorrect', 'hide')
+        if(charIndex > 0) { charIndex-- }
+    }
+    else if(textSpan[textSpan.length-1]?.className){ //마지막 글자가 입력한 값과 같을때 다음버튼 클릭되도록 혹은 클래스가 있을때
+        nextButton.click()
+        console.log('마지막글자')
     }
     else{   // 글자가 불일치할 경우
-        inputDiv.innerText =''
         textSpan[charIndex].classList.add('incorrect')
+        textSpan[charIndex].classList.remove('correct', 'hide')
+        charIndex++
     }
 })
 // 본문 클릭시 textarea 커서 활성화
@@ -147,7 +153,6 @@ bibleText.addEventListener('click',(e)=>{
         index++
         main.replaceChildren()
         await getBibleText()       
-        console.log(bibleText.innerHTML)
     }else if(index == serverData[0].psalms.length - 1){
         alert('마지막 장입니다.')
     }
@@ -172,7 +177,29 @@ retryButton.addEventListener('click',(e)=>{
     getBibleText()
 }
 })
- 
+
+// 셀렉트 옵션넣기
+for(let i=0; i < serverData[0].psalms.length - 1; i++){
+    newArr.push(serverData[0].psalms[i].chapter)
+}
+let chapters = [...new Set(newArr)]
+
+const select = document.createElement('select')
+typingContent.appendChild(select)
+
+for(let i=0; i<chapters.length; i++){
+const option = document.createElement('option')
+option.text = chapters[i]
+select.append(option)
+}
+
+select.addEventListener('change',(e)=>{
+    index = e.target.value
+    console.log(e.target)
+    main.replaceChildren()
+    getBibleText()
+})
+
 }
 (async () => await getBibleText())()
 
